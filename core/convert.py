3f6796bcb5c363b73ae5af955d58a324910a99c3
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-02-04 18:23:32 -0600

    Create convert.py
    Allows conversion from nx-TAS to PyTAS.
    May add automatic function creation in the feature, but that seems like too much of a far cry from reality.

---
+++ core/convert.py
@@ -0,0 +1,50 @@
+import os
+
+class convert():
+    def __init__(self):
+        self.keys = (
+        'NONE',
+        'KEY_A',
+        'KEY_B',
+        'KEY_X',
+        'KEY_Y',
+        'KEY_L',
+        'KEY_R',
+        'KEY_ZL',
+        'KEY_ZR',
+        'KEY_PLUS',
+        'KEY_MINUS',
+        'KEY_DLEFT',
+        'KEY_DUP',
+        'KEY_DRIGHT',
+        'KEY_DDOWN',
+        'KEY_LSTICK',
+        'KEY_RSTICK',
+        )
+
+    def justify(self,inputs:list):
+        text = '# This is an auto-generated PyTAS file\ndef main():\n'
+        char = '\''
+        lastframe = 0
+        for i in inputs:
+            if not i:
+                continue
+            i = i.split(' ')
+            i = {
+            'Frame':int(i[0]),
+            'Key':i[1],
+            'LeftStick':i[2],
+            'RightStick':i[3],
+            }
+            if i['Key'] != 'NONE' or i['LeftStick'] != '0;0' or i['RightStick'] != '0;0':
+                text = f"{text}    script.input({i['Frame'] - lastframe},({','.join([ f'{char}{h}{char}' for h in i['Key'].split(';') ])},),{','.join(i['LeftStick'].split(';'))},{','.join(i['RightStick'].split(';'))})\n"
+            else:
+                text = f"{text}    script.wait({i['Frame'] - lastframe})\n"
+            lastframe = i['Frame']
+        return text
+
+    def convertFromFile(self,FILENAME:str):
+        if not os.path.isfile(FILENAME):
+            raise Exception('Invalid file passed')
+        with open(FILENAME,'r') as file:
+            return self.justify(file.read().split('\n'))
